ffb6e6048bf25a2ae70e95c46eb59fe5c286d1e5
Ethan Cho <ethan.cho1826@gmail.com>
2022-07-18 23:24:15 -0500

    functionality to check for correct answer and keeping score

--- quiz_brain.py
+++ quiz_brain.py
@@ -2,18 +2,25 @@ class Quiz:
     def __init__(self, questions):
         self.questions = questions
         self.current_question = 0
+        self.score = 0
 
     def next_question(self):
-        while True:
-            user_answer = input(f"Q.{self.current_question + 1}: {self.questions[self.current_question].question} (True/False)? ")
-            if user_answer != 'True' and user_answer != 'False':
-                print("Please answer either True or False.")
-                continue
-            else:
-                self.current_question += 1                
-                return user_answer
+            current = self.questions[self.current_question]
+            user_answer = input(f"Q.{self.current_question + 1}: {current.question} (True/False)? ")             
+            self.current_question += 1
+            self.check_answer(user_answer, current.answer)
     
     def has_question(self):
         if self.current_question >= len(self.questions):
             return False
         return True
+
+    def check_answer(self, input, answer):
+        if input.lower() == answer.lower():
+            print("You got it right!")
+            self.score += 1
+        else:
+            print("That's wrong")
+        print(f"The correct answer was {answer}")
+        print(f"Your current score is: {self.score}/{self.current_question}")
+
